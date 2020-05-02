79272c6bbf9d62fdfbfccdf1cf1662aef9055836
Petros Aristidou <apetros@pm.me>
2020-05-02 22:00:43 +0300

    updated copyright date

--- conf.py
+++ conf.py
@@ -61,7 +61,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pyramses'
-copyright = u'2019, Petros Aristidou'
+copyright = u'2020, Petros Aristidou'
 author = u'Petros Aristidou'
 
 # The version info for the project you're documenting, acts as replacement for
